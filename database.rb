a8ebb227e1a5599b0c33c830911227f72f5325ed
Guillaume Claret <dev@clarus.me>
2014-10-29 12:48:16 +0100

    Better table of results

--- database.rb
+++ database.rb
@@ -33,11 +33,18 @@ class Database
     end.sort {|x, y| compare_versions(x, y)}
   end
 
-  # The times of the benches for a given architecture, repository and Coq version.
+  # The times of the benches for a given architecture, repository and Coq
+  # version. At least one time is returned, or an error is raised.
   def times(architecture, repository, coq_version)
-    Dir.glob("#{@folder}/#{architecture}/#{repository}/#{coq_version}/*.csv").map do |name|
+    regexp = "#{@folder}/#{architecture}/#{repository}/#{coq_version}/*.csv"
+    output = Dir.glob(regexp).map do |name|
       Time.strptime(File.basename(name, ".csv"), "%F_%T")
     end.sort.reverse
+    if output == [] then
+      raise "At least on bench expected in #{regexp}."
+    else
+      output
+    end
   end
 
   # The list of tested packages in a bench. A bench is described by an
@@ -46,17 +53,24 @@ class Database
   def packages(architecture, repository, coq_version, time)
     file_name = "#{@folder}/#{architecture}/#{repository}/#{coq_version}/#{time.strftime("%F_%T")}.csv"
     CSV.read(file_name).map do |row|
-      [row[0], row[1], Result.new(row[2..-1])]
+      [row[0][4..-1], row[1], Result.new(row[2..-1])]
     end.sort
   end
 
-  def repository_packages(architecture, repository)
-    coq_versions(architecture, repository).map do |coq_version|
+  # `{name => {version => {coq_version => result}}}`
+  def packages_hash(architecture, repository)
+    output = {}
+    for coq_version in coq_versions(architecture, repository) do
       time = times(architecture, repository, coq_version)[0]
-      packages(architecture, repository, coq_version, time)
+      packages = packages(architecture, repository, coq_version, time)
+      for name, version, result in packages do
+        output[name] = {} if output[name].nil?
+        output[name][version] = {} if output[name][version].nil?
+        output[name][version][coq_version] = result
+      end
     end
+    output
   end
-
 
   # def packages(architecture, repository, coq_version)
   #   (Dir.glob("#{@folder}/#{architecture}/#{repository}/#{coq_version}/*").map do |name|
