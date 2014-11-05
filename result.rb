35e9f9eed8248a1ec85ad27f2aeb99ba25bbb2c6
Guillaume Claret <dev@clarus.me>
2014-11-05 16:12:10 +0100

    Generation of the uninstall results

--- result.rb
+++ result.rb
@@ -4,14 +4,18 @@ class Result
     :dry_with_coq_command, :dry_with_coq_status, :dry_with_coq_duration, :dry_with_coq_output,
     :dry_without_coq_command, :dry_without_coq_status, :dry_without_coq_duration, :dry_without_coq_output,
     :deps_command, :deps_status, :deps_duration, :deps_output,
-    :package_command, :package_status, :package_duration, :package_output
+    :package_command, :package_status, :package_duration, :package_output,
+    :uninstall_command, :uninstall_status, :uninstall_duration, :uninstall_output,
+    :missing_removes, :mistake_removes
 
   def initialize(*arguments)
     @status,
       @dry_with_coq_command, @dry_with_coq_status, @dry_with_coq_duration, @dry_with_coq_output,
       @dry_without_coq_command, @dry_without_coq_status, @dry_without_coq_duration, @dry_without_coq_output,
       @deps_command, @deps_status, @deps_duration, @deps_output,
-      @package_command, @package_status, @package_duration, @package_output =
+      @package_command, @package_status, @package_duration, @package_output,
+      @uninstall_command, @uninstall_status, @uninstall_duration, @uninstall_output,
+      @missing_removes, @mistake_removes =
       arguments
   end
 
@@ -24,7 +28,7 @@ class Result
       "info"
     when "DepsError"
       "warning"
-    when "Error"
+    when "Error", "UninstallError"
       "danger"
     else
       raise "unknown status #{@status.inspect}"
@@ -38,7 +42,7 @@ class Result
       @package_duration.to_i.duration
     when "NotCompatible"
       "OK"
-    when "DepsError", "Error"
+    when "DepsError", "Error", "UninstallError"
       "Error"
     else
       raise "unknown status #{@status.inspect}"
@@ -56,6 +60,8 @@ class Result
       "Error with dependencies"
     when "Error"
       "Error"
+    when "UninstallError"
+      "Uninstallation error"
     else
       raise "unknown status #{@status}"
     end
