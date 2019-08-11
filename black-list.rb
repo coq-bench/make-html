84711fb7b2faffafdccc8a325a7d32805adf2fe3
Guillaume Claret <dev@clarus.me>
2019-08-11 16:45:41 +0200

    Add new black-listed packages

--- black-list.rb
+++ black-list.rb
@@ -2,9 +2,11 @@
 
 BlackList = [
   ["coq-bdds.8.5.0", "Error: Could not find the .cmi file for interface parser.mli."],
+  ["coq-bdds.8.7.0", "Error: Could not find the .cmi file for interface parser.mli."],
   ["coq-bignums.8.6.0", "Error: Corrupted compiled interface"],
   ["coq-bignums.8.7.0", "Error: Unable to locate library NMake_gen."],
   ["coq-coq2html.1.0", "Error: Unbound module Resources"],
+  ["coq-coq2html.1.1", "Error: Unbound module Resources"],
   ["coq-cybele.1.3.0", "make inconsistent assumptions over interface CybeleConstants"],
   ["coq-hammer.1.1.1+8.9", "Error: Corrupted compiled interface"],
   ["coq-ltac2.0.3", "coq-ltac2 -> coq >= 8.10"],
