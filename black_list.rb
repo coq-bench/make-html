1878eeac1ae8c402e1d3d8791456f3fdabc44833
Guillaume Claret <dev@clarus.me>
2019-10-17 11:00:40 +0200

    Remove ltac2 from the black-list

--- black_list.rb
+++ black_list.rb
@@ -4,7 +4,5 @@
 
 BlackList = [
   ["coq-compcert.3.1.0", "Error: Corrupted compiled interface"], # flaky Makefile
-  ["coq-compcert.3.3.0", "Error: Corrupted compiled interface"], # flaky Makefile
-  ["coq-ltac2.0.3", "coq-ltac2 -> coq >= 8.10"], # Coq 8.10 not released yet
-  ["coq-ltac2.0.3", "While removing coq"] # Coq 8.10 not released yet
+  ["coq-compcert.3.3.0", "Error: Corrupted compiled interface"] # flaky Makefile
 ]
