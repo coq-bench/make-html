bd378002e3769793d37a40b98a0ed504ead87bb4
Guillaume Claret <dev@clarus.me>
2019-11-27 14:05:38 +0100

    Add CompCert 3.2.0 to the black-list for flaky Makefile

--- black_list.rb
+++ black_list.rb
@@ -4,6 +4,7 @@
 
 BlackList = [
   ["coq-compcert.3.1.0", "Error: Corrupted compiled interface"], # flaky Makefile
+  ["coq-compcert.3.2.0", "Error: Corrupted compiled interface"], # flaky Makefile
   ["coq-compcert.3.3.0", "Error: Corrupted compiled interface"], # flaky Makefile
   ["coq-compcert.3.6", "Error: Corrupted compiled interface"], # flaky Makefile
   ["coq-stalmarck.8.5.0", "Error: Could not find the .cmi file for interface stal.mli."] # flaky Makefile
