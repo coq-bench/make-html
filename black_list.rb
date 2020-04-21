2ccabd6d5dbd9eef53087ae8e1a1ca165370cc20
Guillaume Claret <dev@clarus.me>
2020-04-21 22:31:48 +0200

    Add compcert 3.0.0 to the black-list

--- black_list.rb
+++ black_list.rb
@@ -4,6 +4,7 @@
 
 BlackList = [
   ["coq-compcert.2.7.1", "Error: Corrupted compiled interface"], # flaky Makefile
+  ["coq-compcert.3.0.0", "Error: Corrupted compiled interface"], # flaky Makefile
   ["coq-compcert.3.1.0", "Error: Corrupted compiled interface"], # flaky Makefile
   ["coq-compcert.3.2.0", "Error: Corrupted compiled interface"], # flaky Makefile
   ["coq-compcert.3.3.0", "Error: Corrupted compiled interface"], # flaky Makefile
