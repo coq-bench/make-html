0a1cf54a57e1396676ac10736ab25497d08c77ed
Guillaume Claret <dev@clarus.me>
2020-12-04 09:45:49 +0100

    Adding coq-compcert.3.0.1 to the list of black-list errors

--- black_list.rb
+++ black_list.rb
@@ -6,6 +6,7 @@ BlackList = [
   ["coq-compcert.2.5.0", "Error: Corrupted compiled interface"],
   ["coq-compcert.2.7.1", "Error: Corrupted compiled interface"],
   ["coq-compcert.3.0.0", "Error: Corrupted compiled interface"],
+  ["coq-compcert.3.0.1", "Error: Corrupted compiled interface"],
   ["coq-compcert.3.1.0", "Error: Corrupted compiled interface"],
   ["coq-compcert.3.2.0", "Error: Corrupted compiled interface"],
   ["coq-compcert.3.3.0", "Error: Corrupted compiled interface"],
